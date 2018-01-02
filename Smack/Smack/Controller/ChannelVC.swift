5fb1ca901a4cf0b2c7ca3b0e2c8660f5e0ad495a
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-01-02 09:26:19 -0500

    Finished lesson-02 ie lesson 65

---
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Chetwyn on 1/2/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+
+}
---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,33 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Chetwyn on 1/2/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+
+        
+    }
+
+
+
+}
