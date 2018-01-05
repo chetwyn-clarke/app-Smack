26cc80882c2b8bd2d0719fb8279c5d84ad7d8ec3
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-01-05 15:16:53 -0500

    Finished lesson 04 or lesson 66

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Chetwyn on 1/5/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Chetwyn on 1/5/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
