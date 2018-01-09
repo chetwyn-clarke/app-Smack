61bc2eb7fa0a67dbd4b141941ba716db94132de2
Chetwyn <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2018-01-09 07:55:41 -0500

    Finished lesson 09 or lesson 71

---
+++ Smack/Smack/Services/AuthServices.swift
@@ -0,0 +1,80 @@
+//
+//  AuthServices.swift
+//  Smack
+//
+//  Created by Chetwyn on 1/9/18.
+//  Copyright © 2018 Clarke Enterprises. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+// This will handle all services for registering and creating a user.
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        //The API is a bit unique in that for this particular task it returns a string, so we have to specify that in the Alamofire request. Normally you would want a JSON return, and would specify that; so you need to know what your API is asking for.
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
-//Segues
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+
+let BASE_URL = "https://chetsmackchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannelVC"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
